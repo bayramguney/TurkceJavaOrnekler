ca124414e7b54fc40ecda7f6ffc691b25e565fcb
bayramguney <bayramguney@yahoo.com>
2020-09-02 00:31:15 -0400

    encapsulation eklendi

---
+++ src/DenemeEncapsulation/Test.java
@@ -0,0 +1,14 @@
+package DenemeEncapsulation;
+
+public class Test {
+    public static void main(String[] args) {
+      /*  Abone abone=new Abone();
+        abone.isim="Mustafa";
+        abone.bakiye=200;
+        abone.sehir="Ankara";
+        abone.dogalgazKullan(200);*/
+
+      AboneGelismis aboneGelismis=new AboneGelismis("mustafa",200,"Ankara");
+      aboneGelismis.bakiyeOgren();
+    }
+}
